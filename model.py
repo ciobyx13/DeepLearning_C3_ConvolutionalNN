48ec8498a67066588669b4533bfbe0c50a014352
Andrei <andrei.cioboata@tquila-automation.com>
2022-10-19 13:38:58 +0200

    added W1A2

---
+++ model.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imread
+import scipy
+from PIL import Image
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+from tensorflow.python.framework import ops
+from cnn_utils import *
+from test_utils import summary, comparator
+
+%matplotlib inline
+np.random.seed(1)
